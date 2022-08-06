32bb94c8f4acf675e9a6412bd77d9044ca30e9ee
Jeff Zapotoczny <jeff.zapotoczny@sonarsource.com>
2022-08-06 11:42:38 -0400

    Removed unused imports.

--- src/test/java/com/sonarsource/springdemo/AppControllerTest.java
+++ src/test/java/com/sonarsource/springdemo/AppControllerTest.java
@@ -1,9 +1,6 @@
 package com.sonarsource.springdemo;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;

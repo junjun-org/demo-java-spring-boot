ba150c31a7795fc3cea1ab4c2ce013c7818f4047
junjun-dachi <junjun.dachi@gmail.com>
2025-02-07 15:18:15 +0800

    fix a spring jpa issue

--- src/main/java/com/sonarsource/springdemo/model/FileEntity.java
+++ src/main/java/com/sonarsource/springdemo/model/FileEntity.java
@@ -1,11 +1,12 @@
 package com.sonarsource.springdemo.model;
 
-import org.springframework.data.annotation.Id;
+
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 
 @Entity
